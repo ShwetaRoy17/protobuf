a22fe08cece33adf4f0a35c34b75402440a7c76a
ShwetaRoy17 <royshweta1712@gmail.com>
2026-02-17 23:55:12 +0530

    added clsoe and read function

--- internal/log/index.go
+++ internal/log/index.go
@@ -1,8 +1,15 @@
 package log
 
 import (
+	"github.com/tysonmote/gommap"
+	"io"
 	"os"
-"github.com/tysonmote/gommap"
+)
+
+var (
+	offWidth uint64 = 4
+	posWidth uint64 = 8
+	entWidth        = offWidth + posWidth
 )
 
 type index struct {
@@ -17,16 +24,16 @@ type Config struct {
 	}
 }
 
-func newIndex(f *os.File, c Config) (*index, error ){
+func newIndex(f *os.File, c Config) (*index, error) {
 	idx := &index{
-		file:f,
+		file: f,
 	}
 	fi, err := os.Stat(f.Name())
 	if err != nil {
 		return nil, err
 	}
 	idx.size = uint64(fi.Size())
-	if err := os.Truncate(f.Name(), int64(c.Segment.MaxIndexBytes),);err!=nil {
+	if err := os.Truncate(f.Name(), int64(c.Segment.MaxIndexBytes)); err != nil {
 		return nil, err
 	}
 	if idx.mmap, err = gommap.Map(
@@ -36,6 +43,40 @@ func newIndex(f *os.File, c Config) (*index, error ){
 	); err != nil {
 		return nil, err
 	}
-	return idx, nil	
+	return idx, nil
 
+}
+
+func (i *index) Close() error {
+	if err := i.mmap.Sync(gommap.MS_ASYNC); err != nil {
+		return err
+	}
+	if err := i.file.Sync(); err != nil {
+		return err
+	}
+	if err := i.file.Truncate((int64(i.size))); err != nil {
+		return err
+	}
+	return i.file.Close()
+}
+
+func (i *index) Read(in int64) (out uint32, pos uint64, err error) {
+	if i.size == 0 {
+		return 0, 0, io.EOF
+	}
+
+	if in == -1 {
+		out = uint32((i.size / entWidth) - 1)
+	} else {
+		out = uint32(in)
+	}
+
+	pos = uint64(out) *entWidth
+	if i.size < pos+entWidth {
+		return 0,0, io.EOF
+	}
+
+	out = enc.Uint32(i.mmap[pos:pos+offWidth])
+	pos = enc.Uint64(i.mmap[pos+offWidth:pos+entWidth])
+	return out, pos, nil
 }
