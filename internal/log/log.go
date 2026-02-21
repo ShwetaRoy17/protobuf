24b28816f1ce4d0e4e3b8ab9852b3b35b05cf62f
ShwetaRoy17 <royshweta1712@gmail.com>
2026-02-22 01:09:24 +0530

    added log file

---
+++ internal/log/log.go
@@ -0,0 +1,75 @@
+package log
+
+import (
+	"io/ioutil"
+	"strings"
+	"sync"
+	"path"
+	"strconv"
+	"sort"
+)
+
+type Log struct {
+	mu sync.RWMutex
+	Dir string
+	Config Config
+	activeSegment *segment
+	segments []*segment
+}
+
+func NewLog(dir string, c Config) (*Log, error ){
+	if c.Segment.MaxStoreBytes == 0 {
+		c.Segment.MaxStoreBytes = 1024
+	}
+
+	if c.Segment.MaxIndexBytes == 0 {
+		c.Segment.MaxIndexBytes = 1024
+	}
+	l:= &Log {
+		Dir:dir,
+		Config:c,
+	}
+	return l, l.setup()
+}
+
+func (l *Log) setup() error {
+files, err := ioutil.ReadDir(l.Dir)
+if err != nil {
+	return err
+}
+var baseOffsets []uint64
+for _, file := range files {
+	offStr := strings.TrimSuffix(
+		file.Name(),
+		path.Ext(file.Name()),
+	)
+	off, _ := strconv.ParseUint(offStr,10,0)
+	baseOffsets = append(baseOffsets, off)
+
+}
+sort.Slice(baseOffsets, func(i, j int) bool {
+	return baseOffsets[i]<baseOffsets[j]
+})
+for i:= 0;i<len(baseOffsets);i++ {
+	if err := l.newSegment(baseOffsets[i]); err != nil {
+		return err
+	}
+	i++
+}
+if l.segments == nil {
+	if err := l.newSegment(l.Config.Segment.InitialOffset); err != nil {
+		return err
+	}	
+}
+return nil 
+}
+
+func (l *Log) newSegment(baseOffset uint64) error {
+	s, err := newSegment(l.Dir, baseOffset, l.Config)
+	if err != nil {
+		return err
+	}
+	l.segments = append(l.segments, s)
+l.activeSegment = s
+return nil
+}
